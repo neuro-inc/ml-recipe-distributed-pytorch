Corrected total training steps number calculation.

modules/model/trainer/trainer.py
@@ -87,11 +87,26 @@ class Trainer:
         self.model = self.model.to(self.device)
         self.loss = self.loss.to(self.device)
 
+        self.train_dataloader = Trainer._init_dataloader(self.train_dataset,
+                                                         'Train',
+                                                         batch_size=int(self.train_batch_size // self.batch_split),
+                                                         n_jobs=self.n_jobs,
+                                                         sampler=self._init_train_sampler(),
+                                                         drop_last=True,
+                                                         collate_fun=self.collate_fun)
+
+        self.test_dataloader = Trainer._init_dataloader(self.test_dataset,
+                                                        'Test',
+                                                        batch_size=self.test_batch_size,
+                                                        n_jobs=self.n_jobs,
+                                                        sampler=None,
+                                                        drop_last=False,
+                                                        collate_fun=self.collate_fun)
+
         self.scheduler = None
-        use_scheduler = self.train_dataset is not None and self.optimizer is not None and self.warmup_coef > 0
+        use_scheduler = self.train_dataloader is not None and self.optimizer is not None and self.warmup_coef > 0
         if use_scheduler:
-            # todo: incorrect value during distributed training?
-            num_training_steps = self.n_epochs * len(self.train_dataset) // self.train_batch_size
+            num_training_steps = self.n_epochs * len(self.train_dataloader) // self.batch_split
             num_warmup_steps = int(num_training_steps * self.warmup_coef)
 
             logger.info(f'Wurmup scheldure is used. #Training steps: {num_training_steps}. '
@@ -115,22 +130,6 @@ class Trainer:
                 )
             else:
                 self.model = torch.nn.parallel.DistributedDataParallel(self.model, find_unused_parameters=True)
-
-        self.train_dataloader = Trainer._init_dataloader(self.train_dataset,
-                                                         'Train',
-                                                         batch_size=int(self.train_batch_size // self.batch_split),
-                                                         n_jobs=self.n_jobs,
-                                                         sampler=self._init_train_sampler(),
-                                                         drop_last=True,
-                                                         collate_fun=self.collate_fun)
-
-        self.test_dataloader = Trainer._init_dataloader(self.test_dataset,
-                                                        'Test',
-                                                        batch_size=self.test_batch_size,
-                                                        n_jobs=self.n_jobs,
-                                                        sampler=None,
-                                                        drop_last=False,
-                                                        collate_fun=self.collate_fun)
 
         self.global_step = 0
         self.writer = Trainer._init_writer(self.local_rank, self.writer_dir)

modules/train.py
@@ -42,6 +42,9 @@ def run_worker(device, params, model_params):
     logger = get_logger(level=log_level, filename=log_file, filemode='a', logger_name='train', debug=params.debug)
 
     logger.warning(f'Process with local_rank: {params.local_rank}. Used device: {device}. GPU id: {gpu_id}.')
+    if params.distributed:
+        logger.warning(f'Batch size will be increased by {params.dist_world_size} times because of distributed '
+                       f'training. Correct your learning rate in the proper way.')
 
     model, tokenizer = init_model(model_params, bpe_dropout=params.bpe_dropout)
     optimizer = init_optimizer(params, model)
@@ -116,7 +119,8 @@ def main(params, model_params) -> None:
 
     set_seed(params.seed)
 
-    # todo: wrong rank if nodes have different gpu number or when node does not have a gpu
+    # Wrong rank if nodes have different gpu number or when node does not have a gpu
+    # Only nodes with the same preset are supported
     params.dist_ngpus_per_node = torch.cuda.device_count()
     params.dist_world_size *= params.dist_ngpus_per_node
     params.distributed = params.dist_world_size > 1