0ae9bdd59bc4782615af399809532054087b67e5
Alexey Naiden <17383+anayden@users.noreply.github.com>
2021-03-22 16:42:53 +0300

    Complete migration (#5)

--- scripts/worker.sh
+++ scripts/worker.sh
@@ -1,8 +1,6 @@
-if [ "$MASTER_IP" == 0 ]
+if [ "$MASTER_IP" == "0" ]
 then
     MASTER_IP="$(hostname).platform-jobs"
 fi
 
-python ./modules/train.py --local_rank $LOCAL_RANK \ --dist_world_size $WORLD_SIZE \
-                          --dist_backend nccl \
-                          --dist_init_method "tcp://${MASTER_IP}:${MASTER_PORT}" "$@"
+python ./modules/train.py --local_rank "$LOCAL_RANK"  --dist_world_size "$WORLD_SIZE" --dist_backend nccl --dist_init_method "tcp://${MASTER_IP}:${MASTER_PORT}" "$@"
